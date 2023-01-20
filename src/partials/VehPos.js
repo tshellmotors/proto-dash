a97e21d7edccaa300410bf82469f6a8659e6a679
gopirajdesigner <89451713+gopirajdesigner@users.noreply.github.com>
2023-01-20 14:51:46 +0530

    update 3d model

--- src/partials/VehPos.js
+++ src/partials/VehPos.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 // import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
-import { Gltf, OrbitControls, Box, Segment, Segments } from "@react-three/drei";
+import {
+  Gltf,
+  OrbitControls,
+  View,
+  PerspectiveCamera,
+  Segment,
+  Segments,
+} from "@react-three/drei";
 
-function Mesh() {
+function Mesh({ rot }) {
   return (
-    <mesh position={[0, 0, 0]} rotation={[0, 45, 0]}>
+    <mesh rotation={rot}>
       <Gltf src="/assets/chassis.glb" />
       <Segments limit={6} lineWidth={2.0}>
         <Segment start={[0, 0, 0]} end={[10, 0, 0]} color={"red"} />
-        <Segment start={[0, 0, 0]} end={[0, 10, 0]} color={"blue"} />
+        {/* <Segment start={[0, 0, 0]} end={[0, 10, 0]} color={"blue"} /> */}
         <Segment start={[0, 0, 0]} end={[0, 0, 10]} color={"green"} />
         <Segment start={[0, 0, 0]} end={[-10, 0, 0]} color={[1, 0, 0]} />
         <Segment start={[0, 0, 0]} end={[0, -10, 0]} color={[0, 1, 0]} />
@@ -22,13 +29,30 @@ function Mesh() {
 const VehPos = () => {
   return (
     <div className="VehPos">
+      <div className="VehPosLabel">
+        <dl>
+          <dt>Yaw</dt>
+          <dd>0 deg</dd>
+        </dl>
+        <dl>
+          <dt>Pitch</dt>
+          <dd>0 deg</dd>
+        </dl>
+        <dl>
+          <dt>Roll</dt>
+          <dd>0 deg</dd>
+        </dl>
+      </div>
       <Canvas>
-        <OrbitControls />
-        <directionalLight intensity={0.5} />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.9} />
+        <PerspectiveCamera makeDefault position={[0, 0, 20]} fov={45} />
+        {/* <OrbitControls /> */}
+        <directionalLight intensity={1} />
+        <spotLight position={[-10, 15, 10]} angle={0.9} />
+        <spotLight position={[10, -15, -10]} angle={0.9} />
 
-        <Mesh />
+        <mesh position={[-1, -2, 0]} rotation={[25.6, -15, 0]}>
+          <Mesh rot={[0, 0, 0]} />
+        </mesh>
       </Canvas>
     </div>
   );
