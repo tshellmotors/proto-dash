a393ba446c99147287a971ddb475fb213fdaba63
gopi raja <gopirajdesigner@gmail.com>
2023-07-03 12:31:01 +0530

    basic setup

--- server/server/index.js
+++ server/server/index.js
@@ -1,38 +1,41 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-const { SerialPort } = require("serialport");
-const { ReadlineParser } = require("@serialport/parser-readline");
-const port = new SerialPort({ path: "COM3", baudRate: 115200 });
-
-const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
-
-io.on("connection", (socket) => {
-  console.log(`user connected: ${socket.id}`);
-
-  parser.on("data", function (data) {
-    socket.emit("sensor_data_m", data);
-  });
-});
-
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
 // });
 
-server.listen(5000, () => {
+// const { SerialPort } = require("serialport");
+// const { ReadlineParser } = require("@serialport/parser-readline");
+// const port = new SerialPort({ path: "COM3", baudRate: 115200 });
+
+// const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+
+// io.on("connection", (socket) => {
+//   console.log(`user connected: ${socket.id}`);
+
+//   parser.on("data", function (data) {
+//     socket.emit("sensor_data_m", data);
+//   });
+// });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+app.listen(5000, () => {
   console.log("server started on port 5000");
 });
+
+// server.listen(5000, () => {
+//   console.log("server started on port 5000");
+// });
--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { useEffect, useState } from "react";
-import "./scss/home.scss";
-import LeftPanel from "./partials/LeftPanel";
-import RightPanel from "./partials/RightPanel";
-import CenterPanel from "./partials/CenterPanel";
-import io from "socket.io-client";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+// import "./scss/home.scss";
 
-const socket = io.connect("http://localhost:5000");
-
-function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    socket.on("sensor_data_m", (data) => {
-      setData(data);
-    });
-  }, [socket]);
-
+export default function App() {
   return (
-    <div className="App">
-      <LeftPanel />
-      <CenterPanel />
-      <RightPanel posData={data} />
+    <div>
+      <h1>Basic Example</h1>
+
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      fgfg
+      <hr />
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
